5bdb200ed3d4c995bbb84c6c11386ef54d9d0ba4
Manuel C. <manuelwn21@gmail.com>
2025-11-23 15:38:00 -0300

    feat(tests): add schema validation for test routes

--- src/modules/test-routes/routes.ts
+++ src/modules/test-routes/routes.ts
@@ -1,11 +1,36 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import z from 'zod';
 
 export async function testRoutes(app: FastifyInstance) {
-	app.get('/', (req: FastifyRequest, res: FastifyReply) => {
-		res.send({ hello: 'world' });
-	});
+	app.get(
+		'/',
+		{
+			schema: {
+				tags: ['Teste'],
+				description: 'Rota de teste com Hello World',
+				response: {
+					200: z.object({ hello: z.string().default('world') }),
+				},
+			},
+		},
+		(req: FastifyRequest, res: FastifyReply) => {
+			res.send({ hello: 'world' });
+		},
+	);
 
-	app.get('/ping', (req: FastifyRequest, res: FastifyReply) => {
-		res.send({ message: 'ping' });
-	});
+	app.get(
+		'/ping',
+		{
+			schema: {
+				tags: ['Teste'],
+				description: 'Rota de teste com Ping',
+				response: {
+					200: z.object({ message: z.string().default('ping') }),
+				},
+			},
+		},
+		(req: FastifyRequest, res: FastifyReply) => {
+			res.send({ message: 'ping' });
+		},
+	);
 }
